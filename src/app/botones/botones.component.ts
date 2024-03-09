eebfd26e766e79f7bf78c0ffe2ef19ffeea4a81b
wilmarcabezas <wilmar.cabezas.pisco@gmail.com>
2024-03-09 18:06:16 -0500

    Binding de Propiedades

--- src/app/botones/botones.component.ts
+++ src/app/botones/botones.component.ts
@@ -11,5 +11,6 @@ export class BotonesComponent {
 
 text_color: string='';
 button_disabled: boolean=true;
+imagen:string='https://images.pexels.com/photos/20440051/pexels-photo-20440051/free-photo-of-moda-gente-mujer-relajacion.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
 
 }
