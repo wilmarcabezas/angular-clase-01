44a3399e634841c4bd315a660274d37407c2af8d
wilmarcabezas <wilmar.cabezas.pisco@gmail.com>
2024-03-10 10:38:20 -0500

    FormBuilder

--- src/app/formulario-reactivo/formulario-reactivo.component.ts
+++ src/app/formulario-reactivo/formulario-reactivo.component.ts
@@ -1,18 +1,39 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { ReactiveFormsModule, Validators } from '@angular/forms';
+import { ReactiveFormsModule, Validators, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-reactivo',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule ],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './formulario-reactivo.component.html',
   styleUrl: './formulario-reactivo.component.css'
 })
 export class FormularioReactivoComponent {
 
-  name = new FormControl('', Validators.required);
-  email=new FormControl('',[Validators.required, Validators.email]);
+  constructor(private fb: FormBuilder){
+
+  }
+
+  get name() {
+    return this.formUser.get('name') as FormControl;
+  }
+  get email() {
+    return this.formUser.get('email') as FormControl;
+  }
+
+  formUser= this.fb.group({
+    'name':['', Validators.required],
+    'email':['', [Validators.required, Validators.email]]
+  })
+  // formUser = new FormGroup({
+  //   name: new FormControl('', Validators.required),
+  //   email: new FormControl('', [Validators.required, Validators.email])
+  // });
+
+  procesar() {
+    console.log(this.formUser.value);
+  }
 
 }
