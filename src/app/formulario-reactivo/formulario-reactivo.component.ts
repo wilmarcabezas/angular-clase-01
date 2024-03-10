0d1d2e33de7801c87eec919934e8ed5103df3f33
wilmarcabezas <wilmar.cabezas.pisco@gmail.com>
2024-03-10 09:54:34 -0500

    Formulario Reactivo con Validators

--- src/app/formulario-reactivo/formulario-reactivo.component.ts
+++ src/app/formulario-reactivo/formulario-reactivo.component.ts
@@ -1,17 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-reactivo',
   standalone: true,
-  imports: [ReactiveFormsModule ],
+  imports: [ReactiveFormsModule, CommonModule ],
   templateUrl: './formulario-reactivo.component.html',
   styleUrl: './formulario-reactivo.component.css'
 })
 export class FormularioReactivoComponent {
 
-  name = new FormControl('Nicolas');
-  lastname=new FormControl('Santiago');
+  name = new FormControl('', Validators.required);
+  email=new FormControl('',[Validators.required, Validators.email]);
 
 }
